6ebf107ccdb8262433b9f62eaccc8a0350aca361
ARCHIT9548 <kietarchit999@gmail.com>
2026-04-15 17:24:14 +0530

    fixed port for render

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,8 @@ const connectDB = require('./src/db/db')
 
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
-
